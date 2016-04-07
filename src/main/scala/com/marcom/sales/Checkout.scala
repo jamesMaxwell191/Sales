ae571a112028dfb2a8120399dad07bbe8509b3ae
dmax <james.maxwell191@gmail.com>
2016-04-07 16:08:21 +0100

    added discount functionality to checkout

--- src/main/scala/com/marcom/sales/Checkout.scala
+++ src/main/scala/com/marcom/sales/Checkout.scala
@@ -16,18 +16,37 @@ class Checkout extends Requirements {
 
                 case items if items.size == 0 => Bad(One("the list of items must not be empty"))
 
-                case items => doPrice(items)
+                case items => priceItems(items)
            }
      }
 
-     def doPrice(items:List[String]) : Int Or One[ErrorMessage] = {
-         if(items.exists(invalid(_))) Bad(One("the list contains invalid items")) else findPrice(items)
+     def priceItems(items:List[String]) : Int Or One[ErrorMessage] = {
+         if(items.exists(invalid(_))) Bad(One("the list contains invalid items")) else findPriceOf(items)
      }
 
-     def findPrice(items:List[String]) = Good(items.map(prices(_)).reduce(_ + _))
+     def findPriceOf(items:List[String]) = {
+         val apples = priceApples(items.filter(_ == "apple").size)
+          val oranges = priceOranges(items.filter(_ == "orange").size)
+          Good(apples + oranges)
+     }
 
      def invalid(item:String) = item == null || (item != "apple" && item != "orange")
 
-     def price(item:String) = 1
+     def priceApples(n:Int):Int = {
+           n match {
+              case  n if n%2 == 0 =>  n/2 * prices("apple")
+
+              case n => (n -1)/2 * prices("apple") + prices("apple")
+           }
+     }
+
+     def priceOranges(n:Int):Int = {
+          n match {
+               case  n if n%3 == 0 =>  n/3 * 2 * prices("orange")
+
+               case n => (n -(n%3))/3 * prices("orange") + (n % 3) *prices("orange")
+          }
+     }
+
 
 }
--- src/test/scala/com/marcom/sales/CheckoutTest.scala
+++ src/test/scala/com/marcom/sales/CheckoutTest.scala
@@ -27,7 +27,27 @@ class CheckoutTest extends WordSpec with Matchers {
           "generate a valid price" in new CheckoutFixture {
                val items = List("apple","orange")
                val result = checkout.price(items)
-               result should be (Good(One(85)))
+               result should be (Good((85)))
+          }
+          "generate a valid discount price for an even basket of apples" in new CheckoutFixture {
+               val items = List("apple","apple")
+               val result = checkout.price(items)
+               result should be (Good((60)))
+          }
+          "generate a valid discount price for an odd basket of apples" in new CheckoutFixture {
+               val items = List("apple","apple","apple")
+               val result = checkout.price(items)
+               result should be (Good((120)))
+          }
+          "generate a valid discount price for a basket of oranges" in new CheckoutFixture {
+               val items = List("orange","orange","orange")
+               val result = checkout.price(items)
+               result should be (Good((50)))
+          }
+          "generate a valid discount price for an odd basket of oranges" in new CheckoutFixture {
+               val items = List("orange","orange")
+               val result = checkout.price(items)
+               result should be (Good((50)))
           }
 
      }
